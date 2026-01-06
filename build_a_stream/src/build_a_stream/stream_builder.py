9a83f4098eb2efd8025ee28a9f0d128e0981ee21
Andrew-Tolbert <andrew.tolbert@databricks.com>
2026-01-06 16:06:41 -0500

    happy 2026

--- build_a_stream/src/build_a_stream/stream_builder.py
+++ build_a_stream/src/build_a_stream/stream_builder.py
@@ -3,15 +3,17 @@ from datetime import datetime
 from databricks.sdk import WorkspaceClient
 
 class Params:
-    def __init__(self, catalog, schema, volume, updated_at):
+    def __init__(self, catalog, schema, volume, updated_at,stream_length_m, files_per_m, reset_data, compression, streaming_mode, streaming_file_size_m):
         self.catalog = catalog
         self.schema = schema
         self.volume = volume
+        self.stream_length_m = stream_length_m
+        self.files_per_m = files_per_m
+        self.reset_data = reset_data
+        self.compression = compression
+        self.streaming_mode = streaming_mode
+        self.streaming_file_size_m = streaming_file_size_m
         self.updated_at = updated_at
-
-    def __repr__(self):
-        return (f"Params(catalog={self.catalog!r}, schema={self.schema!r}, "
-                f"volume={self.volume!r}, updated_at={self.updated_at!r})")
 
 # Create the WorkspaceClient and Params object at the module level
 w = WorkspaceClient()
@@ -20,6 +22,12 @@ params = Params(
     catalog=w.dbutils.widgets.getArgument('catalog'),
     schema=w.dbutils.widgets.getArgument('schema'),
     volume='stream',
+    stream_length_m=float(w.dbutils.widgets.getArgument("stream_length_m")),
+    files_per_m=int(w.dbutils.widgets.getArgument("files_per_m")),
+    reset_data=True if w.dbutils.widgets.getArgument("reset_data") == 'Y' else False,
+    compression=True if w.dbutils.widgets.getArgument("compression") == 'Y' else False,
+    streaming_mode=True if w.dbutils.widgets.getArgument("streaming_mode") == 'Y' else False,
+    streaming_file_size_m=float(w.dbutils.widgets.getArgument("streaming_file_size_m")),
     updated_at=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 )
 
